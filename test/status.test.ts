213e718f665e17a827948fce5feabd8932c356fd
Eliseu Videira <eliseuvideira1@gmail.com>
2020-08-05 23:39:29 -0300

    Added jest for testing

---
+++ test/status.test.ts
@@ -0,0 +1,9 @@
+import { request } from './request';
+
+describe('status', () => {
+  test('GET /status', async () => {
+    expect.assertions(1);
+    const response = await request().get('/status');
+    expect(response.status).toBe(204);
+  });
+});
