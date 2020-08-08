3e1480d2b5090dea49de40283759fd80f35a035f
Eliseu Videira <eliseuvideira1@gmail.com>
2020-08-08 20:44:31 -0300

    Improved toCamel and toSnake functions

--- src/utils/toCamel.ts
+++ src/utils/toCamel.ts
@@ -1,14 +1,8 @@
 export const snakeToCamel = (key: string): string =>
-  key.replace(/_(.?)/g, (_, letter) => letter.toUpperCase());
+  key.replace(/_(.)/g, (_, letter) => letter.toUpperCase());
 
-const mapKeys = (object: any, result: any) => {
-  return (key: string): void => {
-    result[snakeToCamel(key)] = object[key];
-  };
-};
-
-export const toCamel = (object: any): any => {
-  const result: any = {};
-  Object.keys(object).forEach(mapKeys(object, result));
-  return result;
-};
+export const toCamel = (original: Record<string, any>) =>
+  Object.keys(original).reduce(
+    (copy, key) => ({ ...copy, [snakeToCamel(key)]: original[key] }),
+    {},
+  );
--- src/utils/toSnake.ts
+++ src/utils/toSnake.ts
@@ -4,14 +4,8 @@ export const camelToSnake = (key: string): string =>
     (_, lower, upper) => `${lower}_${upper.toLowerCase()}`,
   );
 
-const mapKeys = (object: any, result: any) => {
-  return (key: string): void => {
-    result[camelToSnake(key)] = object[key];
-  };
-};
-
-export const toSnake = (object: any): any => {
-  const result: any = {};
-  Object.keys(object).forEach(mapKeys(object, result));
-  return result;
-};
+export const toSnake = (original: Record<string, any>) =>
+  Object.keys(original).reduce(
+    (copy, key) => ({ ...copy, [camelToSnake(key)]: original[key] }),
+    {},
+  );
