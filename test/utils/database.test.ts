58495dd947a0dcf509466755fdc8df0e5361afb9
Eliseu Videira <eliseuvideira1@gmail.com>
2020-08-09 20:04:31 -0300

    Added testing for database.test.ts

---
+++ test/utils/database.test.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv-safe';
+
+dotenv.config();
+
+import { database } from '../../src/utils/database';
+import {
+  DB_HOST,
+  DB_PORT,
+  DB_USER,
+  DB_PASSWORD,
+  DB_DATABASE,
+} from '../../src/utils/constants';
+
+describe('database', () => {
+  it('uses the environment configuration', () => {
+    expect.assertions(5);
+    const connection = database.client.config.connection;
+    expect(connection.host).toBe(DB_HOST);
+    expect(connection.port).toBe(DB_PORT);
+    expect(connection.user).toBe(DB_USER);
+    expect(connection.password).toBe(DB_PASSWORD);
+    expect(connection.database).toBe(DB_DATABASE);
+  });
+});
