8acf1b1ee57418e24f533303851189712c0259a7
Eliseu Videira <eliseuvideira1@gmail.com>
2020-08-08 21:05:41 -0300

    Added returning types

--- src/utils/toCamel.ts
+++ src/utils/toCamel.ts
@@ -1,7 +1,7 @@
 export const snakeToCamel = (key: string): string =>
   key.replace(/_(.)/g, (_, letter) => letter.toUpperCase());
 
-export const toCamel = (original: Record<string, any>) =>
+export const toCamel = (original: Record<string, any>): Record<string, any> =>
   Object.keys(original).reduce(
     (copy, key) => ({ ...copy, [snakeToCamel(key)]: original[key] }),
     {},
--- src/utils/toSnake.ts
+++ src/utils/toSnake.ts
@@ -4,7 +4,7 @@ export const camelToSnake = (key: string): string =>
     (_, lower, upper) => `${lower}_${upper.toLowerCase()}`,
   );
 
-export const toSnake = (original: Record<string, any>) =>
+export const toSnake = (original: Record<string, any>): Record<string, any> =>
   Object.keys(original).reduce(
     (copy, key) => ({ ...copy, [camelToSnake(key)]: original[key] }),
     {},
