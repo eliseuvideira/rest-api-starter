a08aef135ea223e0a9b7988f8bbe777066247a18
Eliseu Videira <eliseuvideira1@gmail.com>
2020-08-14 20:40:52 -0300

    Removed unused test case

--- test/routes/api-docs.test.ts
+++
@@ -1,9 +0,0 @@
-import { request } from '../request';
-
-describe('api-docs', () => {
-  test('GET /api-docs', async () => {
-    expect.assertions(1);
-    const response = await request().get('/api-docs').redirects(10);
-    expect(response.status).toBe(200);
-  });
-});
