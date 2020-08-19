1b638aa9252a98a3673bc189e33d128c40d931c4
Eliseu Videira <eliseuvideira1@gmail.com>
2020-08-18 23:07:47 -0300

    :wrench: replicating fixes from other repo

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -2,21 +2,19 @@ import { ObjectSchema } from 'joi';
 import { RequestHandler } from 'express';
 import { HttpError } from '../utils/HttpError';
 
-const createRequestValidate = (key: 'body' | 'params' | 'query') => {
-  return (schema: ObjectSchema): RequestHandler => {
-    return (req, _res, next): void => {
-      try {
-        const validationResult = schema.validate(req[key]);
-        if (validationResult.error) {
-          throw new HttpError(400, validationResult.error.message);
-        }
-        req[key] = validationResult.value;
-        next();
-      } catch (err) {
-        next(err);
-      }
-    };
-  };
+const createRequestValidate = (key: 'body' | 'params' | 'query') => (
+  schema: ObjectSchema,
+): RequestHandler => (req, res, next): void => {
+  try {
+    const validationResult = schema.validate(req[key]);
+    if (validationResult.error) {
+      throw new HttpError(400, validationResult.error.message);
+    }
+    req[key] = validationResult.value;
+    next();
+  } catch (err) {
+    next(err);
+  }
 };
 
 export const body = createRequestValidate('body');
