fe8a5d63bda337f72aa1d79a1cd256ca4fd4ad60
Eliseu Videira <eliseuvideira1@gmail.com>
2021-01-27 14:07:16 -0300

    :sparkles: improved server entrypoint

--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,21 @@
-import dotenv from 'dotenv-safe';
+import dotenv from '@ev-fns/dotenv';
 
-dotenv.config();
+dotenv();
 
-console.info(`NODE_ENV is set to ${process.env.NODE_ENV}`);
-console.info(`version ${process.env.npm_package_version}`);
-
-import http from 'http';
+import server from '@ev-fns/server';
 import app from './app';
 import { database } from './functions/database';
 
-const server = http.createServer(app);
-
-const onError = <T extends { syscall: string; code: string }>(err: T): void => {
-  if (err.syscall !== 'listen') {
-    console.error(err);
-    process.exit(1);
-  }
-  switch (err.code) {
-    case 'EACCES':
-      console.error(`Port ${process.env.PORT} requires elevated privileges`);
-      break;
-    case 'EADDRINUSE':
-      console.error(`Port ${process.env.PORT} is already in use`);
-      break;
-    default:
-      console.error(err);
-  }
-  process.exit(1);
-};
-
-const onListening = (): void => {
-  const addr = server.address();
-  if (addr && typeof addr !== 'string') {
-    console.info(`listening at http://localhost:${addr.port}`);
-  }
-};
-
-(async (): Promise<void> => {
-  await database.raw('SELECT 1 AS server_status');
-  server.listen(process.env.PORT);
-  server.on('error', onError);
-  server.on('listening', onListening);
-})().catch((err) => {
+server(
+  app,
+  +(process.env.PORT || 0) || 3000,
+  async () => {
+    await database.raw('SELECT 1 AS server_status');
+  },
+  async () => {
+    console.info(`listening at http://localhost:${process.env.PORT}`);
+  },
+).catch((err) => {
   console.error(err);
   process.exit(1);
 });
