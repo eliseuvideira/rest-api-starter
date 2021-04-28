31263b3452d9e05f3387177e72140c1d5d5f1b97
Eliseu Videira <eliseuvideira1@gmail.com>
2021-04-27 23:02:46 -0300

    :wrench: improved server file

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
 import dotenv from "@ev-fns/dotenv";
 
-dotenv({}, dotenv.startup);
+dotenv({}, ({ NODE_ENV, npm_package_version }) => {
+  console.info(NODE_ENV);
+  console.info(npm_package_version);
+});
 
 import server from "@ev-fns/server";
 import app from "./app";
 
+const PORT = +(process.env.PORT || 0) || 3000;
+
 server({
   app,
-  port: +(process.env.PORT || 0) || 3000,
+  port: PORT,
   after: async () => {
-    console.info(`listening at http://localhost:${process.env.PORT}`);
+    console.info(`http://localhost:${PORT}`);
   },
 }).catch((err) => {
   console.error(err);
