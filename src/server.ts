e9469beb3798d344cf5845d10ec7c188c8078983
Eliseu Videira <eliseuvideira1@gmail.com>
2021-04-27 22:52:05 -0300

    :fire: removed database

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,10 @@ dotenv({}, dotenv.startup);
 
 import server from "@ev-fns/server";
 import app from "./app";
-import { database } from "./functions/database";
 
 server({
   app,
   port: +(process.env.PORT || 0) || 3000,
-  before: async () => {
-    await database.raw("SELECT 1 AS server_status");
-  },
   after: async () => {
     console.info(`listening at http://localhost:${process.env.PORT}`);
   },
