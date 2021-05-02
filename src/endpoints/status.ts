62f9b987204a9f0312808fa4d55699a774c9ae95
Eliseu Videira <eliseuvideira1@gmail.com>
2021-05-01 22:50:27 -0300

    :fire: removed status route

--- src/endpoints/status.ts
+++
@@ -1,5 +0,0 @@
-import { endpoint } from "@ev-fns/endpoint";
-
-export const statusGet = endpoint(async (req, res) => {
-  res.status(204).end();
-});
--- test/routes/status.test.ts
+++
@@ -1,9 +0,0 @@
-import { request } from '../request';
-
-describe('status', () => {
-  test('GET /status', async () => {
-    expect.assertions(1);
-    const response = await request().get('/status');
-    expect(response.status).toBe(204);
-  });
-});
