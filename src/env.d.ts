fc608c0e2736004fdd8ddc1161ad531d1704c2de
Eliseu Videira <eliseuvideira1@gmail.com>
2022-02-12 09:16:46 -0300

    :wrench: modified docker scripts to use bash scripts + fixed environment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,18 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
+    // Environment
     NODE_ENV: "development" | "test" | "production" | "staging";
     PORT: string;
+
+    // Api
     API_NAME: string;
     API_TOKEN: string;
-    API_IMAGE: string;
+
+    // Docker
+    DOCKER_IMAGE: string;
+
+    // Localization
+    LANG: string;
+    TZ?: string;
   }
 }
