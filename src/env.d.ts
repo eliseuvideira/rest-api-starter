32a6c2e81d19e32e47690b8cbdc76cdac181c6d3
Eliseu Videira <eliseuvideira1@gmail.com>
2022-06-11 10:48:35 -0300

    :wrench: small changes

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,18 +1,24 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    // Environment
+    // string(development | test | production | staging), example: development
     NODE_ENV: "development" | "test" | "production" | "staging";
+
+    // number, example: 50001
     PORT: string;
 
-    // Api
+    // string, example: rest-api-starter
     API_NAME: string;
+
+    // string, example: q1M7dW9XHBwtPxvKMzlmrw56Bn7MCg58Kjfb
     API_TOKEN: string;
 
-    // Docker
+    // string, example: registry.server.pw/rest-api-starter
     DOCKER_IMAGE: string;
 
-    // Localization
+    // string(en_US.UTF-8 | pt_BR.UTF-8 | ...), example: en_US.UTF-8
     LANG: string;
+
+    // string(UTC | America/Sao_Paulo | ...), example: UTC
     TZ?: string;
   }
 }
